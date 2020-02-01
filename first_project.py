4e8d4ff810fb1c483dc038fecaccac3fb61abc2c
sujajoshi <joshisujata8@gmail.com>
2020-02-01 17:46:00 +0530

    My_First_Project

---
+++ first_project.py
@@ -0,0 +1,13 @@
+"""
+This is my project!
+New to development. Want to go a long way!
+
+Author : Sujata D Joshi
+Date   : 01-02-2020
+
+"""
+
+name = input("Enter your name:")
+company = input("Enter your company name:")
+
+print("Welcome to ",company, "dear" ,name ,"!")
